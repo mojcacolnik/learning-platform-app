2c9aa9deae09ec4c74b3276682df0cea944eca68
Mojca Colnik <mojca.c13@gmail.com>
2021-04-25 17:59:22 +0200

    Mock data

---
+++ src/app/mock-courses.ts
@@ -0,0 +1,11 @@
+import { Course } from './course';
+
+export const COURSES: Course[] = [
+    {id: 1, name: 'C++'},
+    {id: 2, name: 'C#'},
+    {id: 3, name: 'Python'},
+    {id: 4, name: 'Java'},
+    {id: 5, name: 'Devops'},
+    {id: 6, name: 'JS'},
+    {id: 7, name: 'Angular'},
+]
